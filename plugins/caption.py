6d9b236ad9b616f468a84154f9da1a9fceb9c193
Rehansaputradewantoro <156561840+Rehansaputradewantoro@users.noreply.github.com>
2024-02-10 03:40:04 +0700

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -5,25 +5,25 @@ from helper.database import *
 @Client.on_message(filters.private & filters.command('set_caption'))
 async def add_caption(client, message):
     if len(message.command) == 1:
-       return await message.reply_text("**Give me a caption to set.\n\nExample:- `/set_caption File Name`**")
+       return await message.reply_text("**Mana Teks nya Anjing.\n\nContoh:- `/set_caption File Name`**")
     caption = message.text.split(" ", 1)[1]
     addcaption(int(message.chat.id), caption)
-    await message.reply_text("**Your Caption successfully added ✅**")
+    await message.reply_text("**Nih Teks LOE sudah berhasil ditambahkan ✅**")
 
 @Client.on_message(filters.private & filters.command('del_caption'))
 async def delete_caption(client, message): 
     caption = find(int(message.chat.id))[1]
     if not caption:
-        await message.reply_text("**You dont have any custom caption**")
+        await message.reply_text("**LOE Ngapain Kalo Kagak Punya teks khusus apa pun**")
         return
     delcaption(int(message.chat.id))
-    await message.reply_text("**Your caption successfully deleted ✅**")
+    await message.reply_text("**Nih teks LOE berhasil di hapus ✅**")
                                        
 @Client.on_message(filters.private & filters.command('see_caption'))
 async def see_caption(client, message): 
     caption = find(int(message.chat.id))[1]
     if caption:
-       await message.reply_text(f"<b><u>Your Caption:</b></u>\n\n`{caption}`")
+       await message.reply_text(f"<b><u>Kasih keterangan LOE:</b></u>\n\n`{caption}`")
     else:
-       await message.reply_text("**You dont have any custom caption**")
+       await message.reply_text("**LOE Ngapain Kalo Kagak Punya teks khusus apa pun**")
           
